d623595b1af9ce6bb7b14d4fcdab2cb825eac259
paulesaad <paulsaad.13@gmail.com>
2015-06-18 00:21:14 -0500

    first commit, preparation stages for a pretty cool game

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -5,22 +5,127 @@ require("es5-shim")
 // es6 polyfills, powered by babel
 require("babel/register")
 
-var Promise = require('es6-promise').Promise
-// just Node?
-// var fetch = require('node-fetch')
-// Browserify?
-// require('whatwg-fetch') //--> not a typo, don't store as a var
+var Promise = require('es6-promise').Promise,
+	$ = require('jquery')
 
-// other stuff that we don't really use in our own code
-// var Pace = require("../bower_components/pace/pace.js")
+window.addEventListener('load', app)
 
-// require your own libraries, too!
-// var Router = require('./app.js')
+function app() {
 
-// window.addEventListener('load', app)
+    var Combatant = function(settings) {
+    	var self = this;
 
-// function app() {
-    // start app
-    // new Router()
-// }
+    	if(settings.node) {
+    		this.$node = settings.node
+    		this.node = settings.node[0]
+    	}
 
+    	this.x=0
+    	this.y=0
+    	this.velocity=0
+    	this.canFall = false
+    	this.move_right = false
+    	this.move_left = false
+    	var speed=10
+    	var gravity = .5
+
+    	this.setPosition = function(pos) {
+
+    		if(typeof pos.x==='number')
+    		this.x = pos.x
+
+    		if(typeof pos.y==='number')
+    		this.y = pos.y
+
+    		this.$node.css({
+    			left : this.x + 10 + 'px',
+    			top : $('.canvas').height() - this.$node.height() + this.y - 10 + 'px'
+    		})
+    	}
+
+    	this.getPosition = function(){
+
+			return{
+				x : this.x,
+				y : this.y
+			}
+
+    	}
+    	this.jump = function(){
+    		this.velocity = 20
+    		self.canFall=true;
+    	}
+
+    	this.moveRight = function(){
+    		var next_x_Pos = this.getPosition().x + speed
+
+    		this.setPosition({x:next_x_Pos})
+    	}
+
+    	this.moveLeft = function(){
+    		var next_x_Pos = this.getPosition().x - speed
+
+    		this.setPosition({x:next_x_Pos})
+    	}
+
+    	this.accelerate_y = function(){
+    		var next_y_Pos = self.getPosition().y - this.velocity
+    		if(self.getPosition().y>0)
+    			self.canFall = false
+
+    		if(next_y_Pos>0){
+    			self.canFall = false
+    			next_y_Pos=0
+    		}
+
+    		this.setPosition({y:next_y_Pos})
+    	}
+
+    	;(function ani(){
+    		self.velocity -= gravity
+
+    		if(self.move_right)
+    			self.moveRight()
+    		
+
+    		if(self.move_left)
+    			self.moveLeft()
+
+
+    		if (self.canFall)
+    			self.accelerate_y()
+
+    		requestAnimationFrame(ani)
+    	}())
+
+    	if (typeof settings.x === "number" || typeof settings.y==='number'){
+    		this.setPosition(settings)
+    	}
+
+    }
+
+    var hero = new Combatant({
+    	x:0,
+    	y:0,
+    	node: $('.hero')
+    })
+
+    
+    //loosely coupled system
+    $(document).on('keyup' , function(e){
+    	if(e.keyCode==39)
+    		hero.move_right = false
+    	if(e.keyCode==37)
+    		hero.move_left = false
+	})
+
+    $(document).on('keydown' , function(e){
+    	if(e.keyCode==39)
+    		hero.move_right = true
+    	if(e.keyCode==37)
+    		hero.move_left = true
+    	if(e.keyCode==38 && !hero.canFall)
+    		hero.jump()
+    })
+
+}
